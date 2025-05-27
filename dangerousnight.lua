41b84b1300ede1cb662fb5e9920ba17a5ac100d3
Bac0nH1ckOff <107852416+Bac0nHck@users.noreply.github.com>
2025-05-27 19:15:59 +0300

    Create dangerousnight.lua

---
+++ dangerousnight.lua
@@ -0,0 +1,123 @@
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Turtle-Brand/Turtle-Lib/main/source.lua"))()
+
+local m = lib:Window("Main")
+local t = lib:Window("Teleport")
+local s = lib:Window("Settings")
+
+local players = game:GetService("Players")
+local plr = players.LocalPlayer
+local bunkerName = plr:GetAttribute("AssignedBunkerName")
+
+local lurkerNight = game:GetService("Workspace"):FindFirstChild("LurkerNight")
+
+m:Toggle("Noclip", false, function (b)
+    getgenv().noclip = b
+    if noclip then
+        local function NoclipLoop()
+            if plr.Character ~= nil then
+                for _, child in pairs(plr.Character:GetDescendants()) do
+                    if child:IsA("BasePart") and child.CanCollide == true then
+                        child.CanCollide = false
+                    end
+                end
+            end
+        end
+        Noclipping = game:GetService("RunService").Stepped:Connect(NoclipLoop)
+    else
+        if Noclipping then
+            Noclipping:Disconnect()
+            Noclipping = nil
+        end
+    end
+end)
+m:Box("WalkSpeed", function (ws)
+    if tonumber(ws) then
+        plr.Character:FindFirstChild("Humanoid").WalkSpeed = tonumber(ws)
+    end
+end)
+local lastPos = nil
+m:Button("Collect All Food", function ()
+    lastPos = plr.Character:FindFirstChild("HumanoidRootPart").CFrame
+    for _, food in pairs(game:GetService("Workspace"):GetChildren()) do
+        if food:IsA("Tool") then
+            local handle = food:FindFirstChild("Handle")
+            local prompt = handle:FindFirstChildOfClass("ProximityPrompt")
+            if handle and prompt then
+                plr.Character:FindFirstChild("HumanoidRootPart").CFrame = handle.CFrame * CFrame.new(0,5,0)
+                task.wait(.25)
+                fireproximityprompt(prompt, prompt.MaxActivationDistance)
+            end
+        end
+    end
+    task.wait(.25)
+    plr.Character:FindFirstChild("HumanoidRootPart").CFrame = lastPos
+end)
+m:Toggle("Sound Spam", false, function (b)
+    getgenv().sound_spam = b
+    task.spawn(function ()
+        while sound_spam do
+            game:GetService("ReplicatedStorage"):WaitForChild("SoundEvent"):FireServer("Drink")
+            game:GetService("ReplicatedStorage"):WaitForChild("SoundEvent"):FireServer("Eat")
+            task.wait()
+        end
+    end)
+end)
+m:Toggle("Lurker ESP", false, function (b)
+    getgenv().lurker_esp = b
+    if lurker_esp then
+        task.spawn(function ()
+            while lurker_esp do
+                if lurkerNight then
+                    for _, lurker in pairs(lurkerNight:GetChildren()) do
+                        if lurker:IsA("Model") and lurker:FindFirstChild("HumanoidRootPart") then
+                            local hightlight = lurker:FindFirstChild("Highlight")
+                            if not hightlight then
+                                hightlight = Instance.new("Highlight")
+                                hightlight.Name = "Highlight"
+                                hightlight.Parent = lurker
+                            end
+                        end
+                    end
+                end
+                task.wait(1)
+            end
+        end)
+    else
+        if lurkerNight then
+            for _, lurker in pairs(lurkerNight:GetChildren()) do
+                if lurker:IsA("Model") and lurker:FindFirstChild("HumanoidRootPart") then
+                    local hightlight = lurker:FindFirstChild("Highlight")
+                    if hightlight then
+                        hightlight:Destroy()
+                    end
+                end
+            end
+        end
+    end
+end)
+
+
+t:Button("to Bunker", function ()
+    plr.Character:FindFirstChild("HumanoidRootPart").CFrame = game:GetService("Workspace"):FindFirstChild("Bunkers")[bunkerName]:FindFirstChild("SpawnLocation").CFrame
+end)
+t:Button("to Market", function ()
+    plr.Character:FindFirstChild("HumanoidRootPart").CFrame = CFrame.new(143, 5, -118)
+end)
+t:Box("to Player", function (name, focuslost)
+    if focuslost then
+        local lowerName = name:lower()
+        for _, player in pairs(players:GetPlayers()) do
+            if string.find(player.Name:lower(), lowerName) or string.find(player.DisplayName:lower(), lowerName) then
+                plr.Character:FindFirstChild("HumanoidRootPart").CFrame = player.Character:FindFirstChild("HumanoidRootPart").CFrame
+                return
+            end
+        end
+    end
+end)
+-- Don't change credits bro 💀🙏
+s:Label("Press LeftControl to Hide UI", Color3.fromRGB(127, 143, 166))
+s:Label("~ t.me/arceusxscripts", Color3.fromRGB(127, 143, 166))
+s:Button("Destroy Gui", function ()
+	lib:Destroy()
+end)
+lib:Keybind("LeftControl")
