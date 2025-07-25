c6fc534603d5b693198f70ea155abd03b80684c1
Bac0nH1ckOff <107852416+Bac0nHck@users.noreply.github.com>
2025-07-26 02:53:02 +0300

    Update beachball-farm.lua

--- beachball-farm.lua
+++ beachball-farm.lua
@@ -1,98 +1,18 @@
-local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Turtle-Brand/Turtle-Lib/main/source.lua"))()
-local w = lib:Window("MM2 Summer")
-local plr = game.Players.LocalPlayer
-local character = plr.Character or plr.CharacterAdded:Wait()
-local humPart = character:WaitForChild("HumanoidRootPart")
-plr.CharacterAdded:Connect(function(char)
-    character = char
-    humPart = char:WaitForChild("HumanoidRootPart")
-end)
-local map
-game.Workspace.DescendantAdded:Connect(function(m)
-    if m:IsA("Model") and m:GetAttribute("MapID") then
-        map = m
-    end
-end)
-game.Workspace.DescendantRemoving:Connect(function(m)
-    if m == map then
-        map = nil
-    end
-end)
-local tweenService = game:GetService("TweenService")
-local function teleport(obj)
-    if obj and obj:IsDescendantOf(workspace) and humPart and humPart:IsDescendantOf(workspace) then
-        local distance = (humPart.Position - obj.Position).Magnitude
-        local time = math.clamp(distance / 20, 0.2, 5)
-
-        local tween = tweenService:Create(humPart, TweenInfo.new(time, Enum.EasingStyle.Linear), {CFrame = obj.CFrame * CFrame.new(0, -4, 0)})
-        tween:Play()
-        tween.Completed:Wait(.6)
-
-        local touchInterest = obj:FindFirstChild("TouchInterest")
-        if touchInterest then
-            firetouchinterest(humPart, obj, 0)
-            firetouchinterest(humPart, obj, 1)
-        end
-    end
-end
-getgenv().farm = false
-w:Toggle("BeachBall Farm", false, function(bool)
-    getgenv().farm = bool
-    if not getgenv().farm then
-        if workspace.Gravity ~= 196.2 then workspace.Gravity = 196.2 end
-        return
-    end
-    while getgenv().farm do
-        local container = map and map:FindFirstChild("CoinContainer")
-        if (not map) or (not map.Parent) then
-            for _,m in ipairs(game.Workspace:GetDescendants()) do
-                if m:IsA("Model") and m:GetAttribute("MapID") then
-                    map = m
-                    break
-                end
-            end
-        end
-        if map then
-            if container then
-                if workspace.Gravity ~= 0 then workspace.Gravity = 0 end
-
-                local anyCoin = false
-
-                for _,coin in ipairs(container:GetChildren()) do
-                    if not getgenv().farm then break end
-                    if coin:IsA("Part") and coin.Name=="Coin_Server" and coin:GetAttribute("CoinID")=="BeachBall" then
-                        local cv = coin:FindFirstChild("CoinVisual")
-                        if cv and cv.Transparency~=1 then
-                            anyCoin = true
-                            if not humPart or not humPart.Parent then
-                                humPart = character and character:FindFirstChild("HumanoidRootPart")
-                                if not humPart then break end
-                            end
-                            for _,p in pairs(character:GetChildren()) do
-                                if p:IsA("BasePart") and p.CanCollide then p.CanCollide=false end
-                            end
-                            teleport(coin)
-                        end
-                    end
-                end
-
-                if not anyCoin then
-                    if workspace.Gravity ~= 196.2 then workspace.Gravity = 196.2 end
-                    humPart.CFrame = CFrame.new(93, 140, 61)
-                end
-            end
-        else
-            if workspace.Gravity ~= 196.2 then workspace.Gravity = 196.2 end
-            humPart.CFrame = CFrame.new(93, 140, 61)
-        end
-        task.wait(1)
-    end
-end)
-local GC = getconnections or get_signal_cons
-w:Button("Anti AFK", function()
+pcall(function()
+    repeat wait() until game:IsLoaded()
+    local TeleportService = cloneref(game:GetService("TeleportService"))
+    local Players = game:GetService("Players")
+    local GuiService = cloneref(game:GetService("GuiService"))
+    local placeId = game.PlaceId
+    local plr = Players.LocalPlayer
+    local GC = getconnections or get_signal_cons
     if GC then
-        for _,v in pairs(GC(plr.Idled)) do
-            if v.Disable then v:Disable() elseif v.Disconnect then v:Disconnect() end
+        for _, v in pairs(GC(plr.Idled)) do
+            if v.Disable then
+                v:Disable()
+            elseif v.Disconnect then
+                v:Disconnect()
+            end
         end
     else
         local vu = cloneref(game:GetService("VirtualUser"))
@@ -101,5 +21,66 @@ w:Button("Anti AFK", function()
             vu:ClickButton2(Vector2.new())
         end)
     end
+    GuiService.ErrorMessageChanged:Connect(function()
+        while true do 
+            local suc, err = pcall(function ()
+                TeleportService:TeleportToPlaceInstance(placeId, plr)
+            end)
+            if suc then
+                break
+            else
+                task.wait(2)
+            end
+        end
+    end)
+    plr.CharacterAdded:Connect(function(newChar)
+        char = newChar
+    end)
+    local map = nil
+    game.Workspace.DescendantAdded:Connect(function(m)
+        if m:IsA("Model") and m:GetAttribute("MapID") then
+            map = m
+        end
+    end)
+    game.Workspace.DescendantRemoving:Connect(function(m)
+        if m == map then
+            map = nil
+        end
+    end)
+    while true do
+        if not char or not char:FindFirstChild("HumanoidRootPart") then
+            repeat
+                char = plr.Character
+                task.wait(0.5)
+            until char and char:FindFirstChild("HumanoidRootPart")
+        end
+        while not map or not map:FindFirstChild("CoinContainer") do
+            if char and char:FindFirstChild("HumanoidRootPart") then
+                char.HumanoidRootPart.CFrame = CFrame.new(132, 140, 60)
+            end
+            task.wait(1)
+        end
+        local coinToCollect = nil
+        for _, coin in ipairs(map:FindFirstChild("CoinContainer"):GetChildren()) do
+            if coin:IsA("Part") and coin.Name == "Coin_Server" and coin:GetAttribute("CoinID") == "BeachBall" then
+                local cv = coin:FindFirstChild("CoinVisual")
+                if cv and cv.Transparency ~= 1 then
+                    coinToCollect = coin
+                    break
+                end
+            end
+        end
+        if coinToCollect and char and char:FindFirstChild("HumanoidRootPart") then
+            char.HumanoidRootPart.CFrame = coinToCollect.CFrame
+            task.wait(1)
+            char.HumanoidRootPart.CFrame = CFrame.new(132, 140, 60)
+            task.wait(2)
+        else
+            char.HumanoidRootPart.CFrame = CFrame.new(132, 140, 60)
+            task.wait(1)
+        end
+    end
+    task.spawn(function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/Linux6699/DaHubRevival/main/AntiFling.lua'))()
+    end)
 end)
-w:Label("~ t.me/arceusxscripts", Color3.fromRGB(127, 143, 166))
