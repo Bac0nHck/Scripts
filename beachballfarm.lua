a977b2cf7d052b7b7fc78dcc1e4f9742c46349c9
Bac0nH1ckOff <107852416+Bac0nHck@users.noreply.github.com>
2025-07-26 12:18:43 +0300

    Update beachballfarm.lua

--- beachballfarm.lua
+++ beachballfarm.lua
@@ -4,6 +4,14 @@ local Players = game:GetService("Players")
 local GuiService = cloneref(game:GetService("GuiService"))
 local placeId = game.PlaceId
 local plr = Players.LocalPlayer
+local char = plr.Character or plr.CharacterAdded:Wait()
+local function getValidCharacter()
+	repeat
+		char = plr.Character or plr.CharacterAdded:Wait()
+		task.wait(0.25)
+	until char and char:FindFirstChild("HumanoidRootPart")
+	return char
+end
 local GC = getconnections or get_signal_cons
 if GC then
 	for _, v in pairs(GC(plr.Idled)) do
@@ -109,37 +117,33 @@ game.Workspace.DescendantRemoving:Connect(function(m)
 	end
 end)
 while true do
-	if not char or not char:FindFirstChild("HumanoidRootPart") then
-		repeat
-			char = plr.Character
-			task.wait(0.5)
-		until char and char:FindFirstChild("HumanoidRootPart")
-	end
-	while not map or not map:FindFirstChild("CoinContainer") do
-		if char and char:FindFirstChild("HumanoidRootPart") then
-			char.HumanoidRootPart.CFrame = CFrame.new(132, 140, 60)
-		end
-		task.wait(1)
-	end
-	local coinToCollect = nil
-	for _, coin in ipairs(map:FindFirstChild("CoinContainer"):GetChildren()) do
-		if coin:IsA("Part") and coin.Name == "Coin_Server" and coin:GetAttribute("CoinID") == "BeachBall" then
-			local cv = coin:FindFirstChild("CoinVisual")
-			if cv and cv.Transparency ~= 1 then
-				coinToCollect = coin
-				break
-			end
-		end
-	end
-	if coinToCollect and char and char:FindFirstChild("HumanoidRootPart") then
-		char.HumanoidRootPart.CFrame = coinToCollect.CFrame
-		task.wait(1)
-		char.HumanoidRootPart.CFrame = CFrame.new(132, 140, 60)
-		task.wait(2)
-	else
-		char.HumanoidRootPart.CFrame = CFrame.new(132, 140, 60)
-		task.wait(1)
-	end
+    char = getValidCharacter()
+
+    while not map or not map:FindFirstChild("CoinContainer") do
+        char.HumanoidRootPart.CFrame = CFrame.new(132, 140, 60)
+        task.wait(1)
+    end
+
+    local coinToCollect = nil
+    for _, coin in ipairs(map:FindFirstChild("CoinContainer"):GetChildren()) do
+        if coin:IsA("Part") and coin.Name == "Coin_Server" and coin:GetAttribute("CoinID") == "BeachBall" then
+            local cv = coin:FindFirstChild("CoinVisual")
+            if cv and cv.Transparency ~= 1 then
+                coinToCollect = coin
+                break
+            end
+        end
+    end
+
+    if coinToCollect then
+        char.HumanoidRootPart.CFrame = coinToCollect.CFrame
+        task.wait(1)
+        char.HumanoidRootPart.CFrame = CFrame.new(132, 140, 60)
+        task.wait(2)
+    else
+        char.HumanoidRootPart.CFrame = CFrame.new(132, 140, 60)
+        task.wait(1)
+    end
 end
 task.spawn(function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/Linux6699/DaHubRevival/main/AntiFling.lua'))()
