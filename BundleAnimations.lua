1e24d35c95d52b120a07340ee564c57d900f8720
Bac0nH1ckOff <107852416+Bac0nHck@users.noreply.github.com>
2026-07-13 16:56:50 +0300

    Update comment for script source link

--- BundleAnimations.lua
+++ BundleAnimations.lua
@@ -1,2 +1,2 @@
--- // More scripts: t.me/arceusxcommunity <3
+-- // t.me/arceusxcommunity <3
 loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/2b1a1e4dd23e3419902187e162baf26f.lua"))()
