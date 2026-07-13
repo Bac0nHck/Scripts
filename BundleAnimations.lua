83c9709052f05024415239b0dd512680c3eabd10
Bac0nH1ckOff <107852416+Bac0nHck@users.noreply.github.com>
2026-07-13 16:56:50 +0300

    Update BundleAnimations.lua

--- BundleAnimations.lua
+++ BundleAnimations.lua
@@ -1,1 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Bac0nHck/Scripts/refs/heads/main/BundleAnimationsCore.lua"))()
+-- // More scripts: t.me/arceusxcommunity <3
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/2b1a1e4dd23e3419902187e162baf26f.lua"))()
